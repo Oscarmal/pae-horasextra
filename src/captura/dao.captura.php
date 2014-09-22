b193dd979c2f3bb08ca4ba318ae6813e690e60a0
Oscar Maldonado <oscar.maldonado@isolution.mx>
2014-09-22 10:04:15 -0500

    Se corrige query de insert

--- src/captura/dao.captura.php
+++ src/captura/dao.captura.php
@@ -34,12 +34,12 @@ function captura_insert($auth=0, $id_personal=0, $fecha='', $horas=0){
 		global $db;
 		$timestamp = date('Y-m-d H:i:s');
 		$sql = "INSERT INTO $db[tbl_horas_extra] SET
-					id_personal='$id_personal'
+					id_personal='$id_personal',
 					fecha = '$fecha',
 					horas ='$horas',
 					id_usuario = '$usuario[id_usuario]',
 					timestamp = '$timestamp'
-					LIMIT 1;";
+					;";
 		$resultado = (SQLDo($sql))?true:false;
 	}else{
 		$resultado = false;
