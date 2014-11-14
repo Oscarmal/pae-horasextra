c0166c0841d38735a39ccca6df14bc00df11b3b9
Oscar <oscar.maldonado@isolution.mx>
2014-11-14 14:52:59 -0600

    Se ccorrije menu de contenidos

--- src/views.vars.reportes.php
+++ src/views.vars.reportes.php
@@ -5,7 +5,7 @@
 */
 // Modulo Padre
 global $vistas, $contenidos, $icono;
-$icono = $var[ico_03];
+$icono = $var[ico_04];
 define(MODULO,'REPORTES');
 require_once($Path[src].strtolower(MODULO).'/dao.'.strtolower(MODULO).'.php');
 require_once($Path[src].'build.contenidos.php');
