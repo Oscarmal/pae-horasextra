ab9fd9e3a2bee9d1f071a7a694ea211dd5e02a0a
alex <alvaro.enciso@isolution.mx>
2015-01-13 18:05:22 -0600

    sincronizacion de consulta

--- src/admin/dao.admin.php
+++ src/admin/dao.admin.php
@@ -28,7 +28,7 @@ function select_view_nomina($data=array()){
 		//$filtro.= ($activo)?" and b.activo IN ($activo)":'';
 		$desc 	= ($desc)?" DESC":' ASC';
 		$orden 	= ($orden)?"ORDER BY $orden".$desc:'ORDER BY a.id_empresa, a.id_empleado'.$desc;		
-		$sql="SELECT 
+		/*$sql="SELECT 
 				 a.id_empresa
 				,a.id_number
 				,a.nombre
@@ -40,12 +40,13 @@ function select_view_nomina($data=array()){
 				,a.empresa
 				,a.empresa_razon_social
 				,a.id_empleado
-				/*,IF(a.activo=1,'Activo','Inactivo') AS activo*/
+				/*,IF(a.activo=1,'Activo','Inactivo') AS activo
 				FROM $db[view_nomina] a
 				LEFT JOIN $db[tbl_empresas] b ON a.id_empresa=b.id_empresa
 				WHERE 1
-				$filtro $grupo $orden ;";
-		// dump_var($sql);
+				$filtro $grupo $orden ;";*/
+	
+		 //dump_var($sql);
 		$resultado = SQLQuery($sql);		
 		$resultado = (count($resultado)) ? $resultado : false ;
 	}else{
